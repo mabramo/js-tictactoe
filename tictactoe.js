4b338deca6b383faefd1f5140eaf73f4c4d82ff8
Michael Abramo <abramomichael@gmail.com>
2014-07-02 14:20:56 -0400

    Wrote setup function. BSing in javascript.

--- tictactoe.js
+++ tictactoe.js
@@ -4,4 +4,25 @@
 
 //Get number of players DEFAULT=2
 //Get size of board DEFAULT=[3,3]
-//Get desired length-to-win 
+//Get desired length-to-win
+
+var players;
+var boardx;
+var boardy;
+var numToWin;
+var board = new Array();
+
+function setup()
+{
+  players = 3;
+  boardx = 3;
+  boardy = 3;
+  numToWin = 3;
+
+  board = newArray(boardx);
+  for(i = 0; i < boardx; i++)
+    {
+      board[i] = newArray(boardy);
+    }
+}
+
